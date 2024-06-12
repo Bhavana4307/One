d8acf49bb25180d0575b1a3b65001f1bd900e8f2
bhavana <bhavana.balaguruk@gmail.com>
2024-06-12 14:31:01 +0530

    adding while loop
    Signed-off-by: bhavana <bhavana.balaguruk@gmail.com>

---
+++ src/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        String input = "bhavana";
+        int i = 1;
+        while (i < 10) {
+            System.out.println(input);
+            i++;
+        }
+    }
+}
